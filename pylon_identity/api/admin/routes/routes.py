3dc479a76023b33c7641caa89fd7b38dfa55aeec
marcorsouza <marco.rsouza@gmail.com>
2024-03-25 11:08:19 -0300

    Adicionado testes ao modulo Role

--- pylon_identity/api/admin/routes/routes.py
+++ pylon_identity/api/admin/routes/routes.py
@@ -3,7 +3,10 @@ from fastapi import FastAPI
 from pylon_identity.api.admin.routes.application_routes import (  # Import roteador de usuário
     application_router,
 )
-from pylon_identity.api.admin.routes.user_routes import (  # Import roteador de usuário
+from pylon_identity.api.admin.routes.role_routes import (  # Import roteador de regras
+    role_router,
+)
+from pylon_identity.api.admin.routes.user_routes import (  # Import roteador de aplicações
     user_router,
 )
 
@@ -11,3 +14,4 @@ from pylon_identity.api.admin.routes.user_routes import (  # Import roteador de 
 def add_api_routes(app: FastAPI):
     app.include_router(user_router)
     app.include_router(application_router)
+    app.include_router(role_router)
