e3ea45bec3b6a98ea932d35a6eacc57fb7670d09
marcorsouza <marco.rsouza@gmail.com>
2024-03-25 10:58:36 -0300

    Adicionado testes ao Modulo Application

--- pylon_identity/api/admin/routes/routes.py
+++ pylon_identity/api/admin/routes/routes.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 
+from pylon_identity.api.admin.routes.application_routes import (  # Import roteador de usuário
+    application_router,
+)
 from pylon_identity.api.admin.routes.user_routes import (  # Import roteador de usuário
     user_router,
 )
@@ -7,3 +10,4 @@ from pylon_identity.api.admin.routes.user_routes import (  # Import roteador de 
 
 def add_api_routes(app: FastAPI):
     app.include_router(user_router)
+    app.include_router(application_router)
