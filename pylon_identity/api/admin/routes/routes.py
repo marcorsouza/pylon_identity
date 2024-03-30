b827394b159ab388132388973b9c18f4e4435cb8
Marco Souza <86129076+ws-manz@users.noreply.github.com>
2024-03-30 10:25:46 -0300

    add a variavel de ambiente APPLICATION_FOLDER

--- pylon_identity/api/admin/routes/routes.py
+++ pylon_identity/api/admin/routes/routes.py
@@ -13,6 +13,29 @@ from pylon_identity.api.admin.routes.user_routes import (  # Import roteador de 
     user_router,
 )
 
+"""
+def add_api_routes2(app: FastAPI):
+    
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    
+     # Navega até a raiz do projeto (supondo que a raiz seja o diretório pai do diretório do script)
+    root_dir = os.path.dirname(script_dir)
+    
+    for file in get_files(os.path.join(root_dir, api_folder), '_routes.py', True):
+    
+    # Lista todos os arquivos no diretório de rotas
+    for file in os.listdir(routes_directory):
+        if file.endswith("_routes.py"):  # Filtra por arquivos de rotas
+            module_name = file[:-3]  # Remove a extensão '.py'
+            module_path = f"{base_import_path}.{module_name}"
+            module = importlib.import_module(module_path)
+            for attr_name in dir(module):
+                attr = getattr(module, attr_name)
+                if hasattr(attr, "include_router"):
+                    # Assume que o objeto tem um método 'include_router'
+                    app.include_router(attr)
+"""
+
 
 def add_api_routes(app: FastAPI):
     app.include_router(user_router)
