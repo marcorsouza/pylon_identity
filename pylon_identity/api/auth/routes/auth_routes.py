d0eb209e4ccf97a3f3ab34634150ea1de62905fd
Marco Souza <86129076+ws-manz@users.noreply.github.com>
2024-04-30 23:37:22 -0300

    Documentação da rota Auth

--- pylon_identity/api/auth/routes/auth_routes.py
+++ pylon_identity/api/auth/routes/auth_routes.py
@@ -15,7 +15,13 @@ auth_router = APIRouter(
 )
 
 # Rota de login (sem autenticação real por enquanto)
-@auth_router.post('/login', response_model=TokenAndUserPublic)
+@auth_router.post(
+    '/login',
+    response_model=TokenAndUserPublic,
+    summary='Authenticate a user',
+    description='Performs authentication for a user using username and password credentials. If successful, it returns an access token and user details.',
+    response_description='The authentication token along with public user details.'
+)
 async def login_for_access_token(
     form_data: OAuth2PasswordRequestForm = Depends(),
     auth_controller: AuthController = Depends(get_auth_controller),
@@ -43,7 +49,13 @@ async def login_for_access_token(
     }
 
 
-@auth_router.post('/check-permission', response_model=Message)
+@auth_router.post(
+    '/check-permission',
+    response_model=Message,
+    summary='Check user permissions',
+    description='Checks if the authenticated user has permission to perform a specific action on a resource, based on provided data including username, tag name, acronym, and action name.',
+    response_description='A message indicating if the user has the required permission.'
+)
 async def check_user_permission(
     permission_data: CheckPermissionSchema = Body(...),
     auth_controller: AuthController = Depends(get_auth_controller),
